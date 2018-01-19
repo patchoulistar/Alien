75fd864c2ba9e9eacd8fdb3af099140f38d5120c
chenzijian <chenzijian111@aliyun.com>
2018-01-19 10:33:33 +0800

    将背景设置为灰色

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,9 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    #设置背景色
+    bg_color = (230, 230, 230)
+
     #开始游戏的主循环
     while True:
 
@@ -15,5 +18,12 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        #每次循环时都重绘屏幕
+        screen.fill(bg_color)
+
+        #让最近屏幕可见
+        pygame.display.flip()
+
 
 run_game()
